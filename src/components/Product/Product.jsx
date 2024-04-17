8fcf7da2caceab31ab052ccae3757b0ba19eb4b4
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2024-04-17 09:43:29 +0700

    udated data.js with new projects, updated all links, updated styes

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,6 +1,4 @@
 import styles from "./Product.module.css";
-import GitHub from "../../img/github.png";
-import AppImg from "../../img/app.jpg";
 
 const Product = ({ img, linkGit, linkApp, title, desc, techs }) => {
   return (
@@ -10,19 +8,16 @@ const Product = ({ img, linkGit, linkApp, title, desc, techs }) => {
       </div>
       <div className={styles.descContainer}>
         <div className={styles.projectDescContainer}>
-          {/*  */}
-
           <div className={styles.projectTitle}>{title}</div>
           <div className={styles.projectDesdcription}>{desc}</div>
 
           <div className={styles.ProjectTechs}>
             {techs.map((t) => (
-              <div>{`--${t}`}</div>
+              <div>{`-${t}-`}</div>
             ))}
           </div>
-
-          {/*  */}
         </div>
+
         <div className={styles.projectLinksContainer}>
           <div>
             <a href={linkGit}>code</a>
