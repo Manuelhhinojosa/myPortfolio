1842175d6b6debb50eccf7ffec2e9526b25f7aea
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2024-07-23 16:52:10 -0400

    updated skills component

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -6,8 +6,8 @@ const Skills = () => {
       <div className={styles.skillsTitle}>Skills</div>
       <div className={styles.SkillsList}>
         MongoDB, Mongoose, Atlas MongoDB, Express, React, NodeJS, HTML, CSS,
-        Javascript, Typescript, SASS, public API, EmailJS, Heroku, Netlify,
-        Socket.IO, Cloudinary, GitHub, Git, Terminal, NPM
+        Javascript, SASS, public API, EmailJS, Heroku, Netlify, Cloudinary,
+        GitHub, Git, Terminal, NPM
       </div>
     </div>
   );
