4174b1d65c7336a2c909a98b8d9b47a5fc101d64
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2025-05-19 13:09:25 -0400

    s

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { PROJECTS } from "../constants";
+import { p } from "framer-motion/client";
 
 const Projects = () => {
   return (
     <section className="p-8" id="projects">
       <h2 className="my-10 text-center text-3xl lg:text-8xl">Projects</h2>
-      <div className="columns-1 gap-4 md:columns-2 lg:columns3">
+      <div className="columns-1 gap-4 md:columns-2 lg:columns-3">
         {PROJECTS.map((project) => (
           <a
             key={project.id}
@@ -14,15 +15,16 @@ const Projects = () => {
             rel="noopener noreferrer"
             className="block"
           >
-            <div className="relative mb-4 overflow-hidden rounded-lg bg-white shadow-lg">
+            <div className="relative mb-4 overflow-hidden rounded-lg shadow-lg">
               <img
                 src={project.imgSrc}
                 alt="Project image"
                 className="h-auto w-full object-cover"
               />
-              <div className="absolute bottom-0 left-0 right-0 m-8 p-8 text-white backdrop-blur-md">
-                <h3 className="text-3xl">{project.title}</h3>
-                <p className="max-w-xs text-large"> {project.description}</p>
+
+              <div className="absolute bottom-0 left-0 right-0 top-0 m-8 p-8 text-white backdrop-blur-md bg-black/50 rounded-lg flex flex-col justify-center items-center overflow-hidden overflow-y-scroll md:justify-start lg:justify-start">
+                <h3 className="text-2xl text-center">{project.title}</h3>
+                <p className="text-justify"> {project.description}</p>
               </div>
             </div>
           </a>
--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,14 +1,12 @@
 import { FaGithub, FaLinkedin } from "react-icons/fa6";
 
-import project1 from "../assets/project1.webp";
-import project2 from "../assets/project2.webp";
-import project3 from "../assets/project3.webp";
-import project4 from "../assets/project4.webp";
-import project5 from "../assets/project5.webp";
-import project6 from "../assets/project6.webp";
-import project7 from "../assets/project7.webp";
-import project8 from "../assets/project8.webp";
-import project9 from "../assets/project9.webp";
+import project0 from "../assets/project0.png";
+import project1 from "../assets/project1.png";
+import project2 from "../assets/project2.png";
+import api from "../assets/api.webp";
+import project4 from "../assets/project4.png";
+import project6 from "../assets/project6.png";
+import projectM from "../assets/projectM.png";
 
 export const LINKS = [
   { id: "home", name: "Home" },
@@ -19,12 +17,38 @@ export const LINKS = [
 ];
 
 export const MARQUEE_TEXT =
-  "MERN - MogoDB - MongoDB Atlas - Mongoose - Express JS - React JS - Node JS - REST API creation & integration - Front-end - Back-end - Dev tools (Git, Github) - Hosting services (Netlify, Vercel & Heroku)";
+  " - MERN - MogoDB - MongoDB Atlas - Mongoose - Express JS - React JS - Node JS - REST API creation & integration - Front-end - Back-end - Dev tools (Git, Github) - Hosting services (Netlify, Vercel & Heroku) ";
 
 export const PROJECTS = [
   {
+    id: -2,
+    schoolProject: false,
+    title: "Artist's e-commerce REST API (Back-end)",
+    description: "A fully responsive e-commerce site",
+    imgSrc: api,
+    link: "https://underconstructionmhh.netlify.app",
+  },
+  {
+    id: -1,
+    schoolProject: false,
+    title: "Artist's e-commerce (Client side)",
+    description: "A fully responsive e-commerce site",
+    imgSrc: projectM,
+    link: "https://underconstructionmhh.netlify.app",
+  },
+  {
+    id: 0,
+    schoolProject: false,
+    title: "Cafe & Restaurant Website (Front-end)",
+    description:
+      "A fully responsive website for a local café in Zapopan, built with React, Tailwind CSS, and Framer Motion. The site captures the cozy, community-focused vibe of the café with smooth animations and a clean layout tailored to the client’s vision.",
+    imgSrc: project0,
+    link: "https://github.com/Manuelhhinojosa/cafeZapopanWebsite",
+  },
+  {
     id: 1,
-    title: "Portfolio Website for a Content Writer",
+    schoolProject: false,
+    title: "Portfolio Website for a Content Writer (Front-end)",
     description:
       "Custom portfolio site built for a professional writer using React, TailwindCSS, and Framer Motion. Features include responsive design, smooth animations, and clean, accessible layout.",
     imgSrc: project1,
@@ -33,6 +57,7 @@ export const PROJECTS = [
   {
     id: 2,
     title: "Professional portfolio & Blog (Client side)",
+    schoolProject: true,
     description:
       "Full-stack MERN blog and portfolio site for a real client, with custom admin-only authentication, full CRUD functionality, Cloudinary media handling, and responsive design using React, Express, MongoDB, SASS, and EmailJS.",
     imgSrc: project2,
@@ -40,15 +65,17 @@ export const PROJECTS = [
   },
   {
     id: 3,
+    schoolProject: true,
     title: "Professional portfolio & Blog REST API (Back End)",
     description:
       "Custom-built Node.js and Express API for a client’s blog and portfolio, featuring secure auth with bcrypt, full CRUD for posts, Cloudinary media uploads via multer, and MongoDB Atlas for cloud-hosted data storage.",
-    imgSrc: project3,
+    imgSrc: api,
     link: "https://github.com/Manuelhhinojosa/mariselaFierroPersonalBlogWebisiteAPI",
   },
   {
     id: 4,
     title: "E-Commerce Platform (Client side in development)",
+    schoolProject: true,
     description:
       "Frontend of a custom e-commerce site built with React, Redux, and SASS. Features dynamic cart, Stripe payments, Cloudinary-hosted media, and responsive UI tailored to a real client’s brand.",
     imgSrc: project4,
@@ -57,47 +84,49 @@ export const PROJECTS = [
   {
     id: 5,
     title: "E-Commerce Platform (REST API in development)",
+    schoolProject: true,
     description:
       "Backend REST API for a custom e-commerce platform, built with Node.js, Express, and MongoDB. Features product and order management, Stripe integration, and secure admin routes. Connected to a live frontend and actively maintained.",
-    imgSrc: project5,
+    imgSrc: api,
     link: "https://github.com/Manuelhhinojosa/designStoreE-CommerceBackEnd",
   },
   {
     id: 6,
     title: "Yelpcamp – Full-Stack Campground Review App",
+    schoolProject: true,
     description:
       "A full-stack CRUD web app inspired by Yelp, where users can post, edit, and review campgrounds with location mapping via Mapbox. Built with Express, MongoDB, and Passport.js, this project showcases dynamic routing, full user authentication, and interactive map integration.",
     imgSrc: project6,
     link: "https://github.com/Manuelhhinojosa/yelpcamp",
   },
-  {
-    id: 7,
-    title: "Weather App – Real-Time Forecast",
-    description:
-      "A clean, responsive weather app built with HTML, CSS, and JavaScript that fetches live data from the OpenWeather API. Designed for quick, clutter-free forecasts with dynamic DOM updates and mobile-ready styling — no frameworks, just pure JavaScript and Axios.",
-    imgSrc: project7,
-    link: "https://github.com/Manuelhhinojosa/weatherApp",
-  },
-  {
-    id: 8,
-    title: "Movie Fight – Head-to-Head Movie Comparison App",
-    description:
-      "An interactive app that lets users search and compare two movies side by side using live data from the OMDb API. Built with vanilla JavaScript and Axios, Movie Fight dynamically analyzes stats like box office revenue, awards, and ratings — all in a responsive, fast-loading UI.",
-    imgSrc: project8,
-    link: "https://github.com/Manuelhhinojosa/movieFight",
-  },
-  {
-    id: 9,
-    title: "Maze Game – Physics-Based Puzzle Adventure",
-    description:
-      "A browser-based game built with the Matter.js physics engine, where players navigate through randomly generated mazes. Designed to showcase physics-driven interactions, collision logic, and user-controlled gameplay — all rendered in a smooth, responsive interface.",
-    imgSrc: project9,
-    link: "https://github.com/Manuelhhinojosa/mazeApp",
-  },
+  // {
+  //   id: 7,
+  //   title: "Weather App – Real-Time Forecast",
+  //   description:
+  //     "A clean, responsive weather app built with HTML, CSS, and JavaScript that fetches live data from the OpenWeather API. Designed for quick, clutter-free forecasts with dynamic DOM updates and mobile-ready styling — no frameworks, just pure JavaScript and Axios.",
+  //   imgSrc: project7,
+  //   link: "https://github.com/Manuelhhinojosa/weatherApp",
+  // },
+  // {
+  //   id: 8,
+  //   title: "Movie Fight – Head-to-Head Movie Comparison App",
+  //   description:
+  //     "An interactive app that lets users search and compare two movies side by side using live data from the OMDb API. Built with vanilla JavaScript and Axios, Movie Fight dynamically analyzes stats like box office revenue, awards, and ratings — all in a responsive, fast-loading UI.",
+  //   imgSrc: project8,
+  //   link: "https://github.com/Manuelhhinojosa/movieFight",
+  // },
+  // {
+  //   id: 9,
+  //   title: "Maze Game – Physics-Based Puzzle Adventure",
+  //   description:
+  //     "A browser-based game built with the Matter.js physics engine, where players navigate through randomly generated mazes. Designed to showcase physics-driven interactions, collision logic, and user-controlled gameplay — all rendered in a smooth, responsive interface.",
+  //   imgSrc: project9,
+  //   link: "https://github.com/Manuelhhinojosa/mazeApp",
+  // },
 ];
 
 export const ABOUT =
-  "I'm a Full Stack Developer focused on the MERN stack (MongoDB, Express.js, React.js, Node.js), with experience building responsive, mobile-first web apps and RESTful APIs. I’ve implemented secure authentication, integrated services like Stripe and Cloudinary, and developed accessible, interactive UIs using React, Redux, TailwindCSS, SASS, and Framer Motion. I enjoy translating client needs into real solutions and have helped improve visibility and sales through user-centered design. I’m confident deploying projects with Netlify, Vercel, and Heroku, and I’m looking for a Junior Full Stack, Front-End, or Back-End Developer role where I can keep learning and make an impact.";
+  "I'm a Full Stack Developer focused on the MERN stack (MongoDB, Express.js, React.js, Node.js), with experience building responsive, mobile-first web apps and RESTful APIs. I’ve implemented secure authentication, integrated services like Stripe and Cloudinary, and developed accessible, interactive UIs using React, Redux, TailwindCSS, SASS, and Framer Motion. I enjoy translating client needs into real solutions and have helped improve visibility and sales through user-centered design.";
 
 export const EXPERIENCES = [
   {
@@ -105,23 +134,13 @@ export const EXPERIENCES = [
     role: "Full Stack Software Engineer",
     year: "2024 - Present",
     description:
-      "+ Delivered and deployed multiple full-stack web applications using the MERN stack (MongoDB, Express, React, Node), tailored to real-world needs like blogging and e- commerce.",
+      "+ Delivered and deployed multiple full-stack web applications using the MERN stack, Netlify, Vercel, and Heroku for fast performance and easy updates, integrated rest API services like Stripe, Cloudinary, and EmailJS to streamline workflows.",
     description2:
-      "+ Designed mobile-first, accessible UIs to match each client’s creative vision, improving usability and content reach.",
-    description3:
-      "+ Built secure REST APIs with role-based authentication and integrated services like Stripe, Cloudinary, and EmailJS to streamline workflows.",
+      "+ Designed mobile-first UIs to match each client’s creative vision, improving usability and content reach.",
+    description3: "+ Built secure REST APIs with role-based authentication",
     description4:
       "+ Enabled clients to fully manage their platforms through dynamic CRUD functionality and media uploads, reducing their dependence on developers.",
-    description5:
-      "+ Helped an artist client launch a professional portfolio and blog, resulting in increased visibility, networking opportunities, and a scholarship-funded art residency in Spain.",
-    description6:
-      "+ Built the front end of an e-commerce site using real product images and mock data; client secured early sales through network sharing. Currently developing the API to integrate full functionality.",
-    description7:
-      "+ Deployed applications with Netlify, Vercel, and Heroku for fast performance and easy updates; used environment management tools for scalability.",
-    description8:
-      "+ Maintained clean, well-documented code and provided post-launch support and troubleshooting.",
-    description9:
-      "+ Worked closely with non-technical clients, translating feedback into meaningful design and development updates under real-world constraints.",
+    description5: "+ Provided post-launch support and troubleshooting.",
   },
   {
     company: "Eat with",
